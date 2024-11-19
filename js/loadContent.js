45a6a73f498fb785cdc480db4151d18e00dded26
UlisesRS1 <rankulimarcus@gmail.com>
2024-11-19 14:06:45 -0600

    New pages realized

--- js/loadContent.js
+++ js/loadContent.js
@@ -1,10 +1,15 @@
 function loadHTML(elementId, filePath) {
-    fetch(filePath)
-      .then(response => response.text())
-      .then(data => document.getElementById(elementId).innerHTML = data);
-  }
-  
-  // Carga del NavBar y del Footer
-  loadHTML('navbar', 'html/navbar.html');
-  loadHTML('footer', 'html/footer.html');
-  
+  const basePath = window.location.origin; // Obtiene la base de la URL actual
+  const fullPath = `${basePath}/${filePath}`; // Construye la ruta completa
+  fetch(fullPath)
+    .then(response => {
+      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+      return response.text();
+    })
+    .then(data => document.getElementById(elementId).innerHTML = data)
+    .catch(error => console.error('Error al cargar el archivo:', error));
+}
+
+// Carga del NavBar y del Footer
+loadHTML('navbar', 'html/navbar.html');
+loadHTML('footer', 'html/footer.html');
